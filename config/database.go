625a3dea84fd6ff3db82dff0b58c0a9d428581b7
Lucas França <lucas.franca@tinnova.com.br>
2021-05-30 03:13:04 -0300

    Change database connection to database.go

--- config/database.go
+++ config/database.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 type Database struct {
@@ -25,12 +28,34 @@ func GetDatabaseSettings() Database {
 		fmt.Println("Settings Error", portErr)
 	}
 
-	db := Database{
+	database := Database{
 		Host:     db_host,
 		Port:     db_port,
 		Name:     db_name,
 		User:     db_user,
 		Password: db_password,
 	}
+	return database
+}
+
+var db *gorm.DB
+
+func DatabaseConnection() {
+	settings := GetSettings()
+
+	dsn := fmt.Sprintf("host=%v user=%v password=%v dbname=%v port=%v",
+		settings.db.Host, settings.db.User, settings.db.Password,
+		settings.db.Name, settings.db.Port)
+
+	conn, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db = conn
+}
+
+func GetDatabase() *gorm.DB {
 	return db
 }
