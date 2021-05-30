4af090b02a1f869da6a0a61b82229211a83176ad
Lucas França <lucasfrancaid@gmail.com>
2021-05-30 06:37:13 -0300

    Use package router to init in main

--- main.go
+++ main.go
@@ -2,25 +2,13 @@ package main
 
 import (
 	"go-api/config"
-	"go-api/controllers"
 	"go-api/models"
-
-	"github.com/gin-gonic/gin"
+	"go-api/router"
 )
 
 func main() {
-	r := gin.Default()
 
-	r.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{"message": "pong"})
-	})
-
-	r.GET("/books", controllers.GetAllBooks)
-	r.GET("/books/:id", controllers.GetBook)
-	r.POST("/books", controllers.CreateBook)
-	r.PUT("/books/:id", controllers.UpdateBook)
-	r.DELETE("/books/:id", controllers.DeleteBook)
-
+	r := router.Routes()
 	config.DatabaseConnection()
 	models.Migrator()
 
